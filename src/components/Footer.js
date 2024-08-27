a24656797a3b850b0e011ed3bdf463aff2500f98
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-26 22:01:51 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ModalContext from './ModalContext';
+
 
 function Footer() {
+    const { modalState } = useContext(ModalContext);
     return (
-        <footer>
+        <footer className={modalState.isVisible ? 'hidden' : ''}>
             <div className="social-links">
                 <a href="https://www.linkedin.com/in/jeremy-phares/"><img src={`/img/svg/linkedin.svg`} alt="LinkedIn" /></a>
                 <a href="https://github.com/Jer-Pha"><img src={`/img/svg/github.svg`} alt="GitHub" /></a>
