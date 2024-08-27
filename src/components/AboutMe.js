cf54eb6ea41467c71be52ff5acde00f60ea01c94
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-26 22:01:53 -0700

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function AboutMe() {
+
     return (
-        <section id="about" className="about-me">
-            <h2>About Me</h2>
-            <h2 class="mirror-header">About Me</h2>
+        <section id="about" className="about-me section-body">
+            <h2 data-header="About">About</h2>
             <p>I'm a passionate software developer with a focus on [your area of expertise]. I have [number] years of experience in [relevant technologies]. I'm always eager to learn new things and contribute to innovative projects.</p>
             <img src="your-profile-picture.jpg" alt="Jeremy Phares" />
         </section>
