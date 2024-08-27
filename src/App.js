66d9f09dbb92e27a4114c976ecf6441da84397a1
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-26 22:01:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Hero from './components/Hero';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
+import { ModalProvider } from './components/ModalContext';
+import LightSource from './components/LightSource';
+import useShadowSkew from './components/useShadowSkew';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
@@ -15,18 +18,22 @@ function App() {
     root.classList.toggle('dark-mode', isDarkMode);
   }, [isDarkMode]);
 
+
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  useShadowSkew();
+
   return (
-    <div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+    <ModalProvider><div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <LightSource />
       <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
       <Hero />
-      <AboutMe />
+      {/* <AboutMe /> */}
       <Projects />
       <Footer />
-    </div>
+    </div></ModalProvider>
   );
 }
 
