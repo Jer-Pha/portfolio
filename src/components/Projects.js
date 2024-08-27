51957c902bf2719d9f3f10dc11aab4f9803c4529
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-26 22:01:40 -0700

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 
+
 function Projects() {
     const projects = [
         {
             title: 'Kinda Funny Database',
             slug: 'kfdb',
             description: 'A brief description of your project.',
-            link: 'https://www.kfdb.app/'
+            link: 'https://www.kfdb.app/',
+            imageCount: 5,
         },
         {
             title: 'Survayy',
             slug: 'survayy',
             description: 'A brief description of your project.',
-            link: 'https://www.survayy.com/'
+            link: 'https://www.survayy.com/',
+            imageCount: 5,
         },
     ];
 
     return (
-        <section id="projects" className="projects">
-            <h2>Projects</h2>
-            <h2 class="mirror-header">Projects</h2>
-            <div className="project-cards">
+        <section id="projects" className="projects section-body">
+            <h2 data-header="Projects">Projects</h2>
+            <div id="project-cards">
                 {projects.map((project) => (
                     <ProjectCard key={project.title} project={project} />
                 ))}
