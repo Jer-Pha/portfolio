d7157c2776555cf02034fb52ed8ff4289aec5287
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-26 22:01:49 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import ModalContext from './ModalContext';
 
 function Header({ isDarkMode, toggleDarkMode }) {
     const [isFixed, setIsFixed] = useState(false);
+    const { modalState } = useContext(ModalContext);
 
     useEffect(() => {
         const handleScroll = () => {
             const scrollY = window.scrollY;
             const viewportHeight = window.innerHeight;
-
             const scrollPercentage = (scrollY / (viewportHeight)) * 100;
+            const navLinks = document.querySelectorAll('nav a');
+            const sections = document.querySelectorAll('.section-body');
 
-            setIsFixed(scrollPercentage >= 50);
+            setIsFixed(scrollPercentage >= 85);
+
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                const sectionBottom = sectionTop + section.clientHeight;
+
+                navLinks.forEach((link) => {
+                    const targetId = link.getAttribute('href').substring(1); // Remove '#'
+                    if (targetId === section.id) {
+                        if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+                            link.classList.add('active');
+                        } else {
+                            link.classList.remove('active');
+                        }
+                    }
+                });
+            });
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -21,10 +40,7 @@ function Header({ isDarkMode, toggleDarkMode }) {
     }, []);
 
     return (
-        <header className={`header ${isFixed ? 'fixed' : ''}`}>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
+        <nav className={`header ${isFixed ? 'fixed' : ''} ${modalState.isVisible ? 'hidden' : ''}`}>
             <div id="mode-toggle" onClick={toggleDarkMode}>
                 {isDarkMode ? (
                     <img src="/img/svg/mode-light.svg" alt="Light Mode" />
@@ -32,7 +48,12 @@ function Header({ isDarkMode, toggleDarkMode }) {
                     <img src="/img/svg/mode-dark.svg" alt="Dark Mode" />
                 )}
             </div>
-        </header>
+            <div id="header-links">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+            </div>
+        </nav>
     );
 }
 
