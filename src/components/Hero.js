cc4068d63e42678b954013c9ea66dffa875a6bf1
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-28 16:02:31 -0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Hero() {
+    const [isAtTop, setIsAtTop] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsAtTop(window.scrollY === 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
-        <div id="home" className="hero-view section-body">
+        <div id="home" className={`hero-view section-body ${isAtTop ? 'glowing' : ''}`}>
             <div>
                 <h1>Hello<span className="fade-in">, I'm <span className="hero-name">Jeremy</span>.</span></h1>
             </div>
