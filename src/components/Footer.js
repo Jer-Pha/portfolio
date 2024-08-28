17b54da7ba76f2e9d507ca6d2d2a62d8657edb12
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-08-27 20:23:44 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react';
 import ModalContext from './ModalContext';
-
+import { GitHubIcon, LinkedInIcon } from './Icons';
 
 function Footer() {
     const { modalState } = useContext(ModalContext);
     return (
         <footer className={modalState.isVisible ? 'hidden' : ''}>
             <div className="social-links">
-                <a href="https://www.linkedin.com/in/jeremy-phares/"><img src={`/img/svg/linkedin.svg`} alt="LinkedIn" /></a>
-                <a href="https://github.com/Jer-Pha"><img src={`/img/svg/github.svg`} alt="GitHub" /></a>
+                <a href="https://www.linkedin.com/in/jeremy-phares/"><LinkedInIcon alt="LinkedIn" /></a>
+                <a href="https://github.com/Jer-Pha"><GitHubIcon alt="GitHub" /></a>
             </div>
-            <p>&copy; {new Date().getFullYear()} Jeremy Phares</p>
+            <p>&copy;&nbsp;{new Date().getFullYear()}&nbsp;Jeremy&nbsp;Phares</p>
         </footer>
     );
 }
