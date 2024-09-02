77a868e2fb93f3f6c43dd39947ce21f30ee3780b
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-09-01 22:51:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import About from './components/About';
 import Projects from './components/Projects';
 import { ModalProvider } from './components/ModalContext';
 
+import { adjustVh } from './utils/adjustVh';
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
 
@@ -16,36 +18,24 @@ function App() {
     root.classList.toggle('dark-mode', isDarkMode);
   }, [isDarkMode]);
 
-  useEffect(() => {
-    return () => window.addEventListener('scroll', () => {
-      const nav = document.getElementById('nav-header');
-      const contactSection = document.getElementById('contact');
-      const projectsSection = document.getElementById('projects');
-
-      if (nav && projectsSection) {
-        const navBottom = nav.getBoundingClientRect().bottom;
-        const contactTop = contactSection.getBoundingClientRect().top;
-        const projectsTop = projectsSection.getBoundingClientRect().top;
-
-        if (contactTop <= navBottom) {
-          nav.style.backgroundColor = 'var(--background)';
-        } else if (projectsTop <= navBottom) {
-          nav.style.backgroundColor = 'var(--surface)';
-        } else {
-          nav.style.backgroundColor = 'var(--background)';
-        }
-      }
-    });
-  }, []);
-
-
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
 
+  useEffect(() => {
+    adjustVh(); // Call initially on mount
+    window.addEventListener('resize', adjustVh);
+    return () => {
+      window.removeEventListener('resize', adjustVh);
+    };
+  }, []);
+
   return (
     <ModalProvider><div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-      <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+      <Header
+        toggleDarkMode={toggleDarkMode}
+        isDarkMode={isDarkMode}
+      />
       <Hero />
       <About />
       <Projects />
